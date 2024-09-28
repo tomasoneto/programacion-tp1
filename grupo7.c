890c1ff5347a3c755587f6a77222c259da6e806d
Tomas Oneto <tomas.m.oneto@gmail.com>
2024-09-28 08:21:23 -0300

    Correcciones TO

--- grupo7.c
+++ grupo7.c
@@ -3,9 +3,31 @@
 #include <string.h>
 FILE *t,*p;
 
+/**
+ * Analisis:
+ * La funcion main ejecuta todo el programa.
+ * 
+ * Correcciones:
+ * - Seria conveniente dividir el programa en funciones mas pequeñas, para facilitar la lectura y el mantenimiento del codigo.
+ * - Seria conveniente agregar comentarios que expliquen que hace cada parte del codigo.
+ * - Seria conveniente mantener la indentacion del codigo.
+ * - Seria conveniente agregar un mensaje de error si no se puede abrir el archivo (validar)
+ * - Seria conveniente agregar un mensaje de error si no se puede cerrar el archivo (validar)
+ * - Seria conveniente agregar un mensaje de error si no se puede cargar los datos (validar)
+ * 
+ */
 
 int main()
 {
+
+	/**
+	 * Analisis:
+	 * Define las estructuras de pacientes y telemetrias.
+	 * 
+	 * Correcciones:
+	 * - Seria conveniente mover las definiciones fuera de la funcion main, para que puedan ser utilizadas en otras funciones.
+	 * 
+	 */
 //definiciones
 typedef struct pacientes
 	{
@@ -22,6 +44,40 @@ typedef struct telemetrias
 	unsigned char C;
 	}tele;
 
+	/**
+	 * Analisis:
+	 * Define las variables necesarias para el programa.
+	 * 
+	 * Las variables son:
+	 * - r: Variable para almacenar el resultado de la lectura de los datos de telemetria.
+	 * - i: Variable para iterar sobre los datos de telemetria.
+	 * - l: Variable para almacenar el tamaño del archivo de telemetria.
+	 * - la: Variable para almacenar la cantidad de registros de pacientes.
+	 * - re: Variable para almacenar el resultado de la lectura de los datos de pacientes.
+	 * - pax: Variable para almacenar la cantidad de registros de pacientes.
+	 * - tel: Variable para almacenar la cantidad de registros de telemetria.
+	 * - b: Variable para iterar sobre los registros de pacientes.
+	 * - c: Variable para almacenar el resultado de la comparacion de cadenas.
+	 * - con: Variable para contar la cantidad de coincidencias de apellido.
+	 * - coi: Variable para contar la cantidad de coincidencias de nombre.
+	 * - aux: Variable auxiliar.
+	 * - ce: Variable para almacenar el resultado de cerrar el archivo.
+	 * - x: Variable para iterar sobre los registros de telemetria.
+	 * - promFC: Variable para almacenar el promedio de la frecuencia cardiaca.
+	 * - promPAS: Variable para almacenar el promedio de la presion arterial sistolica.
+	 * - promPAD: Variable para almacenar el promedio de la presion arterial diastolica.
+	 * - promC: Variable para almacenar el promedio de la temperatura corporal.
+	 * - ca: Variable para almacenar la cantidad de registros de telemetria.
+	 * - z: Variable para almacenar la opcion elegida por el usuario.
+	 * 
+	 * 
+	 * Correcciones:
+	 * - Seria conveniente utilizar nombres mas descriptivos para las variables.
+	 * - Los promedios podrian ser float, para tener mayor precision.
+	 * - Las variables de iteracion se pueden declarar en el lugar de cada bucle para mayor claridad.
+	 * 
+	 */
+
 int r,i,l,la,re, pax, tel,b,c,con,coi,aux,ce,x, promFC, promPAS, promPAD,promC,ca,z=1; //SE INICIALIZó Z
 char ap[16], np[32];
 unsigned int nhp;
@@ -30,6 +86,21 @@ paci *pac;
 
 //iniciar programa
 
+/**
+ * Analisis:
+ * Abre los archivos pacientes.bin y telemetria.bin y carga los datos en las estructuras. Se utilizan las funciones fseek y ftell
+ * para obtener el tamaño del archivo y la cantidad de registros.
+ * Se definen los vectores a y nom para almacenar los indices de los pacientes que coinciden con el apellido y nombre ingresados.
+ * 
+ * Correcciones:
+ * - Para volver al principio del archivo, se puede utilizar la funcion fseek con SEEK_SET.
+ * - Seria conveniente definir las variables en la parte superior del programa o medida que se utilizan para mantener consistencia.
+ * - Seria conveniente validar el resultado de malloc
+ * - Seria conveniente explicar la function de los vectores a y nom. Mas adelante se utilizan para almacenar
+ * los indices de los pacientes que coinciden con el apellido y nombre ingresados. Sin embargo se declarar del tamaño de pax,
+ * que es la cantidad de registros de pacientes y eso incumple la consigna de utilizar la memoria necesaria.
+ * 
+ */
 
 //abrir archivo pacientes
 
@@ -98,11 +169,28 @@ r = fread (telem, sizeof (tele), tel, t);
 		
 //cerrar archivos
 
+/**
+ * Analisis:
+ * Cierra los archivos pacientes.bin y telemetria.bin.
+ * 
+ * Correcciones:
+ * - No es necesario asignar el resultado de fclose a una variable.
+ * - Se puede utilizar if else para mayor claridad.
+ */
+
 ce = fclose (t);
 if (ce!=0) {printf("Error al cerrar archivo telemetria.bin\n");}
 ce = fclose (p);
 if (ce!=0) {printf ("Error al cerrar archivo pacientes.bin\n");}
 if (ce == 0) {printf ("Archivos cerrados con exito\n");}
+
+/**
+ * Analisis:
+ * Calcula los valores promedio de los signos vitales.
+ * 
+ * Correcciones:
+ * - No contempla el caso en que no haya registros de telemetria y se divida por 0.
+ */
 
 //calculo valores promedio
 sum=0;
@@ -141,6 +229,14 @@ promC=sum/tel;
 
 //elijo que quiero hacer
 
+/**
+ * Analisis:
+ * Muestra un menu con las opciones disponibles y solicita al usuario que elija una opcion.
+ * 
+ * Correcciones:
+ * - Se podria utilizar un switch para mayor claridad.
+ */
+
 while (z!=0) 
 	{
 	printf("Elija una de las siguientes opciones:\n\n1.Buscar un paciente y mostrar su informacion\n2.Mostrar valor promedio de todos los signos vitales\n3.Mostrar que pacientes se encuentran excedidos de temperatura\n0.Cerrar el programa\n");
@@ -149,6 +245,17 @@ while (z!=0)
 		{
 		//buscar por apellido
 
+
+		/**
+		 * Analisis:
+		 * Solicita al usuario que ingrese apellido, nombre e historia clinina y busca los pacientes
+		 * que coinciden con el apellido ingresado.
+		 * 
+		 * Correcciones:
+		 * - Lo solicitado en la consiga es utilizar solo el apellido como criterio de busqueda
+		 * - Al ramificar el programa para coi = 1 y coi > 1 se repite el codigo, se podria simplificar.
+		 * 
+		 */
 
 		con=0;
 		printf ("introduzca el apellido que desea buscar:\n");
@@ -164,6 +271,8 @@ while (z!=0)
 					}
 				}
 		printf ("se encontraron %d coincidencias\n", con);
+
+
 		if (con>1)
 			{
 			printf("introduzca el nombre/s del paciente:\n");
@@ -271,6 +380,18 @@ while (z!=0)
 	else if (z==3)
 		{
 		//reporte
+
+		/**
+		 * Analisis:
+		 * Muestra los pacientes cuya temperatura difiere en un 5% o mas respecto de la temperatura promedio.
+		 * 
+		 * Correcciones:
+		 * - Falta generar el archivo
+		 * - Falta limitar la impresion a 10 resultados
+		 * - El calculo del umbral se puede realizar una sola vez antes del bucle.
+		 * - Se contempla unicamente el caso en que la temperatura este por encima del promedio + 5% y no el caso inferior.
+		 * 
+		 */
 
 	for (x=0;x<tel;x++)
 		{
